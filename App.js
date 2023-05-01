730b58bcc6aa2479874240a593a1f0ca84b42e76
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-05-01 16:26:53 +0300

    "orientation": "default"

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ const App = () => {
 
   return (
     <>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
     </>
   );
 };
